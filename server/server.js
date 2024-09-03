9d4de09259c61b80554e5c435eec71c0c6fdac2a
Thobani660 <zondit660@gmail.com>
2024-09-03 15:21:56 +0200

    sss

--- server/server.js
+++ server/server.js
@@ -6,6 +6,51 @@ const app = express();
 app.use(cors());
 app.use( morgan("tiny"));
 app.use(express.json());
+
+
+app.get( '/', (req , res) => {
+    const book = {id: 2023, 
+                  Tittle:"Philasande",
+                  Publisher:"SamkeloZondi",
+                  
+                }
+    res.json(book.Tittle)
+    res.send('Philasto ')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(5000, ()=> {
                       console.log("server run at http://localhost:5000")
                    } )
+
+
